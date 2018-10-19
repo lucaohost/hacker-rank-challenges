078581eff50ac3948ed3620d6146cfa6f7453414
lucaxrl <lucas.reginatto.de.lima@gmail.com>
2018-10-19 00:28:06 -0300

    removing if and adding natural limit in for

--- codes/adjacentElementsProduct.php
+++ codes/adjacentElementsProduct.php
@@ -9,12 +9,9 @@ adjacentElementsProduct([-23, 4, -3, 8, -12]);
 function adjacentElementsProduct($inputArray) {
     $largestProduct = $inputArray[0] * $inputArray[1];
     $sizeArray = count($inputArray) - 1;
-    foreach ($inputArray as $key => $number) {
-        if ($key + 1 > $sizeArray) {
-            break;
-        }
-        if ($number * $inputArray[$key + 1] > $largestProduct) {
-            $largestProduct = $number * $inputArray[$key + 1];
+    for ($i=0; $i<$sizeArray; $i++) {       
+        if ($inputArray[$i] * $inputArray[$i + 1] > $largestProduct) {
+            $largestProduct = $inputArray[$i] * $inputArray[$i + 1];
         }
     }
     return $largestProduct;
