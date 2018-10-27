103028c450ffdc870bec02edb484c45723dabebb
lucaxrl <lucas.reginatto.de.lima@gmail.com>
2018-10-27 19:23:26 -0300

    adding other new attemp of improve almostIncriasingSequence

--- codes/almostIncreasingSequence.php
+++ codes/almostIncreasingSequence.php
@@ -3,27 +3,24 @@
  *Given a sequence of integers as an array, determine whether it is possible to obtain a strictly 
  *increasing sequence by removing no more than one element from the array.
  */
-$sequence = [1, 3, 2, 1];
+$sequence = [1, 3, 2];
 almostIncreasingSequence($sequence);
 
 function almostIncreasingSequence($sequence) {
     $sizeSequence = count($sequence);
-    for($j = 0; $j < $sizeSequence;$j++){
-        $increasingOk = true;
-        $testArray = $sequence;
-        unset($testArray[$j]);
-        foreach($testArray as $number){
-            $currentNumber = current($testArray);
-            $nextNumber = next($testArray);
-            if($nextNumber !== false){
-                prev($testArray);
-                if($currentNumber >= $nextNumber){
-                    $increasingOk = false;
-                    break;
-                }
+    for($i=0;$i<$sizeSequence;$i++){
+        $almostIncreasingSequence = true;
+        $newArray = $sequence;
+        unset($newArray[$i]);
+        unset($prevNumber);
+        foreach($newArray as $number){
+            if(isset($prevNumber) && $prevNumber >= $number){
+                $almostIncreasingSequence = false;
+                break;
             }
+            $prevNumber = $number;
         }
-        if($increasingOk){
+        if($almostIncreasingSequence){
             return true;
         }
     }
