1f28aa9a5a8d3b6063b28c535e0caff9fdaf7b0c
lucaxrl <lucas.reginatto.de.lima@gmail.com>
2018-10-28 20:39:06 -0300

    last improve in almostIncreasingSequence

--- codes/almostIncreasingSequence.php
+++ codes/almostIncreasingSequence.php
@@ -7,31 +7,18 @@ $sequence = [1, 2, 1, 2];
 almostIncreasingSequence($sequence);
 
 function almostIncreasingSequence($sequence) {
-      $numOfChanges = 0;
-      $neededChanges = 0;
-      foreach($sequence as $key => $number){
-            if(isset($prevNumber) && $prevNumber >= $number){
-                  $numOfChanges++;
-                  if(array_key_exists($key-2,$sequence)){
-                        if($sequence[$key-2] >= $number){
-                              $neededChanges++;         
-                        }
-                  }
-                  if(array_key_exists($key+1,$sequence)){
-                        if($prevNumber >= $sequence[$key+1]){
-                              $neededChanges++;         
-                        }
-                  }
-                  if($numOfChanges > 1 || $neededChanges > 1){
-                        return false;
-                  }
-                  $neededChanges = 0;
-            }
-            $prevNumber = $number;
-      }
-      if($numOfChanges === 1){
-            return true;
-      }else{
-            return false;
-      }
+  $numOfChanges = 0;
+  $neededChanges = 0;
+  foreach($sequence as $key => $number){
+    if(isset($prevNumber) && $prevNumber >= $number){
+      $numOfChanges++;
+      if(array_key_exists($key-2,$sequence) && $sequence[$key-2] >= $number) $neededChanges++;  
+      if(array_key_exists($key+1,$sequence) && $prevNumber >= $sequence[$key+1]) $neededChanges++;   
+      if($numOfChanges > 1 || $neededChanges > 1) return false;
+        $neededChanges = 0;
+    }
+    $prevNumber = $number;
+  }
+  if($numOfChanges === 1) return true;
+  return false;
 }
